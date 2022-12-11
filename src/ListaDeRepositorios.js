e966e7084f96c235478fc4c588604c3111937140
Hilton M. Cardoso <hilton.m.cardoso@gmail.com>
2022-12-11 12:25:35 -0300

    listagem de repositórios (hcmendes)

---
+++ src/ListaDeRepositorios.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+function ListaDeRepositorios() {
+  const [repositorio, setRepositorio] = useState([]);
+
+  useEffect(() => {
+    async function carregaRepositorios() {
+      const resposta = await fetch(
+        'https://api.github.com/users/hcmendes/repos'
+      );
+      const repositorios = await resposta.json();
+      setRepositorio(repositorios);
+    }
+    carregaRepositorios();
+  }, []);
+
+  return (
+    <ul>
+      {repositorio.map((repositorio) => (
+        <li key={repositorio.id}>{repositorio.name}</li>
+      ))}
+    </ul>
+  );
+}
+export default ListaDeRepositorios;
